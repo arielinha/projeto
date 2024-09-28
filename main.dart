7c81e4ad18da40ad7b0ecc49d2338d5c38b88494
arielinha <161093794+arielinha@users.noreply.github.com>
2024-09-27 21:59:20 -0300

    main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,17 @@
-w
+import 'package:flutter/material.dart';
+import 'salgados_page.dart'; // Importa o arquivo da lista de salgados
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Salgados App',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: ListaSalgados(), // Define a página inicial do app
+    );
+  }
+}
